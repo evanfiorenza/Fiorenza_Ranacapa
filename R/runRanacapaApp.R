386063f5c881ed6435299c69c0e36ab191f481d8
Susannah Liu <susannahliu03@gmail.com>
2025-01-30 13:03:31 -0800

    added option of setting port manually

--- R/runRanacapaApp.R
+++ R/runRanacapaApp.R
@@ -1,11 +1,20 @@
-#' Run the shiny app!
-#' @export
+# ' Run the shiny app!
+# ' @export
 runRanacapaApp <- function() {
+  port <- Sys.getenv("RANACAPA_PORT")
+  port <- as.numeric(port)
+  
   appDir <- system.file("explore-anacapa-output", package = "ranacapa")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `ranacapa`.", call. = FALSE)
   }
+  if (is.null(port) || is.na(port) || port == "")
+  {
+    shiny::runApp(appDir, display.mode = "normal")
 
-  shiny::runApp(appDir, display.mode = "normal")
+  }
+  else
+  {
+    shiny::runApp(appDir, port = port, display.mode = "normal")
+  }
 }
-
